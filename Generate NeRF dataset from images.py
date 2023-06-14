9343a43b2fd9082762594132c3c17eed99cf9edc
ianvol <42355277+ianvol@users.noreply.github.com>
2023-06-14 20:04:23 +0100

    Update Generate NeRF dataset from images.py

--- Generate NeRF dataset from images.py
+++ Generate NeRF dataset from images.py
@@ -1,8 +1,11 @@
-import numpy as np
+import sys
+import argparse
+from llff.poses.pose_utils import gen_poses
 from PIL import Image
 import os
 from tkinter import Tk, filedialog
 from tkinter import messagebox
+import numpy as np
 
 # Create a Tkinter root window
 root = Tk()
@@ -19,11 +22,9 @@ default_focal_length = 50.0  # Replace with your desired default focal length
 max_image_size = (1024, 1024)  # Maximum width and height for resizing
 
 def generate_poses(num_images):
-    # Replace this with your own method to generate poses
-    poses = []
-    for _ in range(num_images):
-        pose = np.random.random(7)  # Generate a random pose
-        poses.append(pose)
+    scenedir = args.scenedir  # Path to the scene directory
+    match_type = args.match_type  # Type of matcher used
+    poses = gen_poses(scenedir, match_type, num_images)
     return poses
 
 poses = generate_poses(len(image_filenames))
@@ -36,16 +37,16 @@ for file in image_files:
         image = Image.open(image_file)
         # Preprocess the image as desired (e.g., resize, crop, normalize, etc.)
         image.thumbnail(max_image_size)
-        images.append(image)
+        images.append(np.array(image))
         heights.append(image.height)
         widths.append(image.width)
 
-poses = np.array(poses)
-images = np.array([np.array(image) for image in images])
-heights = np.array(heights)
-widths = np.array(widths)
+poses = np.array(poses).reshape(-1, 7)
+images = np.array(images).reshape(-1, images[0].shape[0] * images[0].shape[1] * images[0].shape[2])
+heights = np.array(heights).reshape(-1, 1)
+widths = np.array(widths).reshape(-1, 1)
 
-focal = np.full(len(image_filenames), default_focal_length)
+focal = np.full(len(image_filenames), default_focal_length).reshape(-1, 1)
 
 # Prompt the user to select the save path and dataset name for the dataset
 save_path = filedialog.asksaveasfilename(title="Save NeRF Dataset", defaultextension=".npz")
